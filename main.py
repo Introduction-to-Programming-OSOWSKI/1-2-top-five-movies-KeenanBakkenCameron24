f4ac39eb9476f82dd4cd9df007b3a579ff39321a
KeenanBakkenCameron24 <828469@mystma.org>
2021-12-16 09:32:51 -0600

    4

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def top5Movies(a, b, c, d, e):
+def topFiveMovies(a, b, c, d, e):
     return a +" "+ b +" "+ c +" "+ d +" "+ e
 
-top5Movies("Halloween", "Frozen", "Venom", "Star Wars", "Midway") 
+topFiveMovies("Halloween", "Frozen", "Venom", "Star Wars", "Midway") 
